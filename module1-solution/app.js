7d167b8531ef3158dc55eedce7c2c6ebf37c13a4
GROUPINFRA\karensofia.perez <karensofia.perez@cgi.com>
2022-05-24 18:14:33 +0200

    Assignment 1 version 1

---
+++ module1-solution/app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+    $scope.items = "";
+
+    $scope.clickButton = function(){
+     var listItems = $scope.items.split(',');
+
+      $scope.message = function() {
+        if(listItems.length === 1){
+          document.getElementById("textbox").style.border = "2px solid red";
+          document.getElementById("optionalMessage").style.color = "red";
+          return "Please enter data first";
+        } else {
+          document.getElementById("textbox").style.border = "2px solid green";
+          document.getElementById("optionalMessage").style.color = "green";
+          if(listItems.length <= 3){
+            return "Enjoy!";
+          }else {
+            return "Too Much!";
+          }
+        }
+      }
+
+    };
+
+}
+
+})();
