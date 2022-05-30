9db756f83576dd5af56fa22ced73c39410ad1507
GROUPINFRA\karensofia.perez <karensofia.perez@cgi.com>
2022-05-30 18:02:30 +0200

    New files from assignment 2

---
+++ module2-solution/app.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+
+}
+
+function ShoppingListCheckOffService(){
+  var items = [];
+
+  
+}
+
+})();
