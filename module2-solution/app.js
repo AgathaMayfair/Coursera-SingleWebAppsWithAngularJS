7a592004bbd33d9f2c0151797d10c848d05274ce
GROUPINFRA\karensofia.perez <karensofia.perez@cgi.com>
2022-05-31 12:37:21 +0200

    Assignment 2 complete

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -9,17 +9,68 @@ angular.module('ShoppingListCheckOff', [])
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService){
 
+  var toBuyItem = this;
+
+  toBuyItem.showItemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+
+  toBuyItem.addToBoughtList = function(index) {
+    ShoppingListCheckOffService.addToBoughtList(index);
+  };
+
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService){
 
+  var boughtItem = this;
+
+  boughtItem.showItemsAlreadyBought = ShoppingListCheckOffService.getItemsBought();
+
 }
 
 function ShoppingListCheckOffService(){
-  var items = [];
 
-  
+  var service = this;
+
+  var listOfItemsToBuy = [
+    {
+      name: "Tomatoes",
+      quantity: "5"
+    },
+    {
+      name: "Lettuce",
+      quantity: "1"
+    },
+    {
+      name: "Tuna cans",
+      quantity: "2"
+    },
+    {
+      name: "Celerys",
+      quantity: "3"
+    },
+    {
+      name: "Raisins box",
+      quantity: "1"
+    }
+  ];
+
+  var listOfItemsBought = [];
+
+  service.getItemsToBuy = function() {
+    return listOfItemsToBuy;
+  };
+
+  service.getItemsBought = function() {
+    return listOfItemsBought;
+  };
+
+  service.addToBoughtList = function (index) {
+    listOfItemsBought.push(listOfItemsToBuy[index]);
+    listOfItemsToBuy.splice(index, 1);
+  };
+
+
 }
 
 })();
